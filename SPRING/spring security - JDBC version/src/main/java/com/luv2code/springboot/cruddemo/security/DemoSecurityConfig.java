72284b90cd02863dc4201e14afc1f3a53bb5f0ab
Rajdeep Banik <rbanik2212@gmail.com>
2024-09-24 00:09:07 +0100

    Spring security

---
+++ SPRING/spring security - JDBC version/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -0,0 +1,64 @@
+package com.luv2code.springboot.cruddemo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+public class DemoSecurityConfig {
+//    adding JDBC
+
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource){
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+
+
+
+
+//        only need the below two if using custom tables for authentication
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select user_id, pw, active from  members where user_id=?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select user_id, role from roles where user_id=?"
+        );
+
+
+        return jdbcUserDetailsManager;
+    }
+
+     @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+
+        );
+
+//   tell spring we are using basic authentication
+http.httpBasic(Customizer.withDefaults());
+
+//disable CSRF
+//   not required for stateless REST APIs that use POST, PUT and DELETE
+    http.csrf(csrf -> csrf.disable());
+
+return http.build();
+     }
+
+
+}
