6653a61e2f94475c32d0de86a0cc00b656e6fd9a
Rajdeep Banik <rbanik2212@gmail.com>
2024-10-14 21:56:11 +0100

    binary search tree traversal

--- algo&data/structureandalgo/src/Trees/BinarySearchTree.java
+++ algo&data/structureandalgo/src/Trees/BinarySearchTree.java
@@ -1,6 +1,10 @@
 package Trees;
 
-class Node{
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
     int value;
     Node left;
     Node right;
@@ -13,51 +17,50 @@ class Node{
 public class BinarySearchTree {
     Node root;
 
-    public boolean insert(int value){
-        Node newNode =new Node(value);
+    public boolean insert(int value) {
+        Node newNode = new Node(value);
 
-        if(root == null){
+        if (root == null) {
             root = newNode;
             return true;
         }
         Node tmp = root;
 
-        while(true){
-            if(newNode.value == tmp.value){
+        while (true) {
+            if (newNode.value == tmp.value) {
                 return false;
             }
-            if(newNode.value < tmp.value){
-                if(tmp.left ==null){
+            if (newNode.value < tmp.value) {
+                if (tmp.left == null) {
                     tmp.left = newNode;
                     return true;
                 }
                 tmp = tmp.left;
-            }else{
-                if(tmp.right ==null){
+            } else {
+                if (tmp.right == null) {
                     tmp.right = newNode;
                     return true;
                 }
-                tmp= tmp.right;
+                tmp = tmp.right;
             }
 
         }
 
     }
 
-    public boolean contains(int value){
-        if(root == null){
+    public boolean contains(int value) {
+        if (root == null) {
             return false;
         }
 
         Node tmp = root;
 
-        while(tmp != null){
-            if(tmp.value > value){
+        while (tmp != null) {
+            if (tmp.value > value) {
                 tmp = tmp.left;
-            }
-            else if(tmp.value < value){
+            } else if (tmp.value < value) {
                 tmp = tmp.right;
-            }else{
+            } else {
                 return true;
             }
 
@@ -65,5 +68,85 @@ public class BinarySearchTree {
         return false;
     }
 
+    public ArrayList<Integer> BFS() {
+        Node curretNode = root;
+        Queue<Node> queue = new LinkedList<>();
+        ArrayList<Integer> results = new ArrayList<>();
+        queue.add(curretNode);
+
+        while (queue.size() > 0) {
+            curretNode = queue.remove();
+            results.add(curretNode.value);
+            if (curretNode.left != null) {
+
+                queue.add(curretNode.left);
+            }
+            if (curretNode.right != null) {
+
+                queue.add(curretNode.right);
+            }
+        }
+
+        return results;
+    }
+
+
+    public ArrayList<Integer> DFSPreOrder() {
+        ArrayList<Integer> results = new ArrayList<>();
+
+        class Traverse {
+            Traverse(Node currentNode) {
+                results.add(currentNode.value);
+                if (currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
+
+
+    public ArrayList<Integer> DFSPostOrder() {
+        ArrayList<Integer> results = new ArrayList<>();
+
+        class Traverse {
+            Traverse(Node currentNode) {
+                if (currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                if (currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+                results.add(currentNode.value);
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
+
+
+    public ArrayList<Integer> DFSInOrder() {
+        ArrayList<Integer> results = new ArrayList<>();
+
+        class Traverse {
+            Traverse(Node currentNode) {
+                if (currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                results.add(currentNode.value);
+                if (currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
 
 }
+
+
